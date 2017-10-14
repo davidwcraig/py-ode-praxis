156479dd0f8b13db97e4d92d871dd30a20fd7283
David W Craig <jblxdwc@gmail.com>
2017-10-14 18:43:13 +0000

    Comment cleanup.

--- osc-drive-damp.py
+++ osc-drive-damp.py
@@ -1,18 +1,12 @@
 # coding: utf-8
-
 # # This is a test notebook
-
-# In[9]:
 
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 get_ipython().magic('matplotlib inline')
 
-
 # ## Driven damped harmonic oscillator 
-
-# In[21]:
 
 def dosc(y,t,b,w0,f):
     """driven dampled harmonic oscillator derivs
@@ -37,44 +31,19 @@ def g(t):
 # plt.plot(t, sol[:,0])
 # plt.savefig("osc.svg")
 
-
-# In[22]:
-
 t = np.linspace(0,30*np.pi, 1000)   #independent variable array
-
-
-# In[23]:
-
 y0 = [0.5, -0.04] # initial [x,v]
-
-
-# In[26]:
 
 sol = odeint(dosc, y0,t,args=(0.1,1.05,g))
 
-
-# In[27]:
-
 plt.plot(t, sol[:,0])
 
-# In[34]:
-
+# Now define a better drive function and run again, e. g. showing transient.
 def drive(t,a,w):
     """driving function, indep var t, amplitude a, ang freq w"""
     return a*np.cos(w*t)
 
-
-# In[43]:
-
 y0 = [0,0] # initial [x,v]
-
-
-# In[44]:
-
 sol = odeint(dosc, y0,t,args=(0.02,1.05,lambda x: drive(x,0.05,1.03)))
 
-
-# In[45]:
-
 plt.plot(t, sol[:,0])
-
